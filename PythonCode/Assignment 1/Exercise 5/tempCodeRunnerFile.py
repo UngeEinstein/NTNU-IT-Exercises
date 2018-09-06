73986ec515421224fbd3961c74e13d4ce9b43603
kharann <anhkhav@gmail.com>
2018-09-06 21:58:55 +0200

    lol

--- PythonCode/Assignment 1/Exercise 5/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-flyttall_1=float(input("Skriv inn et flytall:"))
-flyttall_2=float(input("Skriv inn enda et flyttall:"))
-flyttall_3=float(input("Skriv inn et siste flyttall:"))
-print("Konvertert til heltall blir det:",int(flyttall_1), int(flyttall_2), int(flyttall_3))
-heltall=int(input("Skriv inn et heltall:"))
-print("Konvertert til flytall blir det:", float(heltall)
