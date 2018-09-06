728351af278abd945734e976881f02ca0994b406
kharann <anhkhav@gmail.com>
2018-09-06 21:57:56 +0200

    Comment

--- PythonCode/Assignment 1/Exercise 5/5A.py
+++ PythonCode/Assignment 1/Exercise 5/5A.py
@@ -1,3 +1,4 @@
+#Learning to use float and int
 flyttall_1=float(input("Skriv inn et flytall:"))
 flyttall_2=float(input("Skriv inn enda et flyttall:"))
 flyttall_3=float(input("Skriv inn et siste flyttall:"))
