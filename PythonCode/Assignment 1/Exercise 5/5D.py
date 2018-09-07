1ae5ec54648732901d2369638958259ff1759014
kharann <anhkhav@gmail.com>
2018-09-07 18:05:58 +0200

    5D finish

---
+++ PythonCode/Assignment 1/Exercise 5/5D.py
@@ -0,0 +1,3 @@
+pineapple=float(input("Skriv et flyttall:"))
+pizza=int(input("Hvor mange desimaler er ønskelig?"))
+print("Tallet du skrev er",pineapple,"og med",pizza,"desimaler blir det til",round(pineapple,pizza))
--- PythonCode/Assignment 1/Exercise 5/tempCodeRunnerFile.py
+++ PythonCode/Assignment 1/Exercise 5/tempCodeRunnerFile.py
@@ -1,4 +1,2 @@
-print("Vennligst gi inn et flyttall med minst 5 siffer både før og etter.")
-tall=input("Hva er tallet ditt? ")
-print("Konvertert til heltall med int(): ")
-print(int(tall))
+import emoji
+print(":triumph:")
